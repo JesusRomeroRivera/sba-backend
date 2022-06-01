ef0fac08c240d9a6bd9817ae50aca7498c264740
ShoLee01 <miguelalfaro2017@gmail.com>
2022-06-01 12:55:40 -0500

    fix: DefaultConnection

--- SBA-BACKEND/Startup.cs
+++ SBA-BACKEND/Startup.cs
@@ -42,7 +42,7 @@ namespace SBA_BACKEND
 
             services.AddDbContext<AppDbContext>(options =>
             {
-                options.UseMySQL(Configuration.GetConnectionString("SmarterAspMySQLConnection"));
+                options.UseMySQL(Configuration.GetConnectionString("DefaultConnection"));
                 //options.UseMySQL(Configuration.GetConnectionString("SmarterAspMySQLConnection"));
             });
 
